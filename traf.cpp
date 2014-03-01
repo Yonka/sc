56d91a3b310eb09b76ccead84b7d1fd134577727
yonka <yonkaps@gmail.com>
2014-03-02 01:59:04 +0400

    format+refact

--- traf.cpp
+++ traf.cpp
@@ -2,7 +2,7 @@
 #include "cstdlib"
 #include "ctime"
 
-class writeI: virtual public sc_interface
+class writeI : virtual public sc_interface
 {
 public:
 	virtual void write(sc_uint<8> data) = 0;
@@ -15,15 +15,16 @@ public:
 	sc_uint<8> x;
 	sc_event send;
 	int runs;
+
 	SC_HAS_PROCESS(trafgen);
+
 	trafgen(sc_module_name mn, int param = 1) : sc_module(mn), runs(param)
 	{
 		srand(time(0));
 		SC_THREAD(gen_event);
-		SC_METHOD(send_byte);
 		sensitive << send;
-
 	}
+
 	void gen_event()
 	{
 		for (int r = 0; r < runs; r++)
@@ -31,17 +32,18 @@ public:
 			for (int i = 0; i < 19; i++)
 			{
 				x = (sc_uint<8>)rand() % 255;
-				send.notify();
-				wait(5, SC_NS);	
+				send_byte();
 			}
 			x = 255;
-			send.notify();
-			wait(5, SC_NS);	
+			send_byte();
 		}
 	}
+
 	void send_byte()
 	{
 		out_port->write(x);
+		send.notify(5, SC_NS);
+		wait();	
 //		cout << "send " << x << '\n';
 	}
 };
@@ -52,7 +54,9 @@ public:
 	sc_fifo<sc_uint<8> > tmp_buf;
 	sc_uint<8> tmp_byte;
 	sc_event eop;
+
 	SC_HAS_PROCESS(receiver);
+
 	receiver(sc_module_name mn) : sc_module(mn), tmp_buf(40)
 	{
 		SC_METHOD(printer);
@@ -64,7 +68,7 @@ public:
 		if (data == 255)
 			eop.notify(0, SC_MS);
 		else 
-		tmp_buf.write(data);
+			tmp_buf.write(data);
 	}
 
 	void printer()
@@ -76,10 +80,13 @@ public:
 		cout << '\n';
 	}
 };
-int sc_main(int argc, char* argv[]) {
+
+int sc_main(int argc, char* argv[]) 
+{
 	int k = 1;
 	if (argc == 2)
 		k = atoi(argv[1]);
+
 	trafgen t("t", k);
 	receiver r("r");
 
